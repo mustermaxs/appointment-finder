f2f11dccfc42d7ff037e381d765165bfdb5ef807
mustermaxs <max.sinnl@hotmail.com>
2023-04-04 22:28:43 +0200

    added appointment id to href for forwarding

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 $(window).on("load", () => {
+  // Get a reference to the spinner element
+  const spinner = $("#spinner");
 
-    // Get a reference to the spinner element
-    const spinner = $('#spinner');
+  $.ajax({
+    type: "GET",
+    url: "./api/appointment/",
+    dataType: "json",
+    beforeSend: () => spinner.show(),
+    success: ({ data }) => {
+      const container = $(".row");
 
-    $.ajax({
-        type: "GET",
-        url: "./api/appointment/",
-        dataType: 'json',
-        beforeSend: () => spinner.show(),
-        success: ({ data }) => {
-
-
-            const container = $('.row');
-
-            data.forEach(appointment => {
-
-                const cardHtml = `
+      data.forEach((appointment) => {
+        const cardHtml = `
                 <div class="col-md-6">
                   <div class="card mb-3">
                     <div class="card-header p-3 text-black">
@@ -24,19 +20,17 @@ $(window).on("load", () => {
                     <div class="card-body">
                       <p class="card-text">Status: ${appointment.status}</p>
                       <p class="card-text">open till: ${appointment.expirationDate}</p>
-                      <a href="appointment.html" class="btn btn-success">vote</a>
+                      <a href="appointment.html#id=${appointment.appointmentId}" class="btn btn-success">vote</a>
                     </div>
                   </div>
                 </div>
               `;
 
+        container.append(cardHtml);
+      });
+    },
+    error: (res) => alert(JSON.stringify(res)),
 
-                container.append(cardHtml);
-            });
-        },
-        error: (res) => alert(JSON.stringify(res)),
-
-        complete: () => spinner.hide()
-    });
-
+    complete: () => spinner.hide(),
+  });
 });
