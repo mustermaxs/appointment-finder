f5a09e96d3c1a3570c8254e7dd3d31a31a6df0d6
mustermaxs <max.sinnl@hotmail.com>
2023-04-04 12:46:27 +0200

    comment api

--- api/controllers/CommentController.php
+++ api/controllers/CommentController.php
@@ -8,14 +8,27 @@ class CommentController extends BaseController
     {
         $this->model = new CommentModel();
     }
-
+    // get all comments for appointment with id
     public function get()
     {
+        // $data = $this->getPostData();
+        $appointmentId = intval($this->request["id"]);
+        $comments = $this->model->getCommentsByAppointmentId($appointmentId);
+        $this->successResponse("", $comments);
     }
 
-    // TODO
+    // add new comment to appointment with id
     public function post()
     {
         $data = $this->getPostData();
+        $content = $data->content;
+        $userId = intval($data->userId);
+        $appointmentId = intval($data->appointmentId);
+        $commentId = $this->model->addCommentByAppointmentId($content, $userId, $appointmentId);
+        if ($commentId >= 1) {
+            $this->successResponse("added comment", null, 200);
+        } else {
+            $this->errorResponse("adding comment failed", 400);
+        }
     }
 }
--- api/models/CommentModel.php
+++ api/models/CommentModel.php
@@ -4,17 +4,35 @@ require_once getcwd() . "/api/Model.php";
 class CommentModel extends Model
 {
     // TODO
-    public function getCommentByAppointmentId($appointmentId)
+    public function getCommentsByAppointmentId($appointmentId)
     {
+        $query =
+            "SELECT c.content, c.createdOn, c.userId, u.userName, c.appointment, a.appointmentDate AS appointmentId FROM comments c
+        JOIN users u ON u.userId = c.userId
+        JOIN appointments a on a.appointmentId = c.appointment
+        WHERE c.appointment = ?;";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("d", $appointmentId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $comments = $result->fetch_all(MYSQLI_ASSOC);
+
+        return $comments;
     }
 
-    // TODO
-    public function getOptionsById($appointmentId)
+    public function addCommentByAppointmentId(string $content, int $userId, int $appointmentId)
     {
-    }
+        $query =
+            "INSERT INTO comments
+        (content, userId, appointment)
+        VALUES(?, ?, ?);";
 
-    // TODO
-    public function getAllAppointments()
-    {
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("sdd", $content, $userId, $appointmentId);
+        $stmt->execute();
+        $commentId = $this->conn->insert_id;
+
+        return $commentId;
     }
 }
