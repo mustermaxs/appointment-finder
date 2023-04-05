94c21292a190ef29730c828fb87c913ad11077e1
Markus Rösner <markus.roesner@gmx.net>
2023-04-05 21:03:45 +0200

    sorted appointsments, added createdOn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+function formatDateWithHours(inputDate) {
+  const date = new Date(inputDate);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${year}.${month}.${day} ${hours}:${minutes}`;
+}
+
 $(window).on("load", () => {
   // Get a reference to the spinner element
   const spinner = $("#spinner");
@@ -10,12 +21,14 @@ $(window).on("load", () => {
     success: ({ data }) => {
       const container = $(".row");
 
-      data.forEach((appointment) => {
+      const reversedData = data.reverse();
+
+      reversedData.forEach((appointment) => {
         const cardHtml = `
                 <div class="col-md-6">
                   <div class="card mb-3">
                     <div class="card-header p-3 text-black">
-                      <h5 class="card-title mb-0">${appointment.title}</h5>
+                      <h5 class="card-title mb-0">${appointment.title}<span style="float:right; font-size:1rem" >${formatDateWithHours(appointment.createdOn)}</span></h5>
                     </div>
                     <div class="card-body">
                       <p class="card-text">Status: ${appointment.status}</p>
